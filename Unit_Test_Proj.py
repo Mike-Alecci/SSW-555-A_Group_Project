638c33e3d9a1403c0dad174b9b539b3449b3c71b
Kyle Gensheimer <kylegensheimer@gmail.com>
2018-09-26 20:47:40 -0400

    wip

--- Unit_Test_Proj.py
+++ Unit_Test_Proj.py
@@ -8,8 +8,10 @@ class ProjectTest(unittest.TestCase):
     def test_marr_div_before_death(self):
         """Tests that the marr_div_before_death method works properly, the first tests assertraises tests a bad 
         GEDCOM file, the following four checks are just based off fake dictionary info added"""
+        cwd = os.path.dirname(os.path.abspath(__file__)) #gets directory of the file
+        file_name = cwd + "\Bad_GEDCOM_test_data.ged"
         with self.assertRaises(ValueError):
-            AnalyzeGEDCOM("Bad_GEDCOM_test_data.ged", False)    #File contains both marr and div before death
+            AnalyzeGEDCOM(file_name, False)    #File contains both marr and div before death
         test_ind_dict = {1 : Individual(), 2: Individual()}
         test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].fams = "Fake Person 1", None, 1 
         test_ind_dict[2].name, test_ind_dict[2].deat, test_ind_dict[2].fams = "Fake Person 2", datetime.datetime.strptime("9 MAR 1998", "%d %b %Y").date(), 1
@@ -34,6 +36,34 @@ class ProjectTest(unittest.TestCase):
         self.assertEqual(CheckForErrors(test_ind_dict, test_fam_dict).marr_div_before_death(), None)  
         #Here the husb is dead but after marriage, this should have no errors and run through the function and return None
 
+    def test_normal_age(self):
+        """Tests that the normal_age method works properly, the first tests assertraises tests a bad 
+        inputs, the following checks are just based off fake dictionary info added"""
+        #Tests age is 1000
+        test_ind_dict = {1 : Individual()} #creates dictionary of individuals
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2001", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1001", "%d %b %Y").date() 
+        test_ind_dict[1].update_age()
+        with self.assertRaises(ValueError): 
+            CheckForErrors(test_ind_dict, {}).normal_age()
+        #Tests age is 150
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1850", "%d %b %Y").date() 
+        test_ind_dict[1].update_age()
+        with self.assertRaises(ValueError):     
+            CheckForErrors(test_ind_dict, {}).normal_age()
+        #Tests age is 149
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1851", "%d %b %Y").date() 
+        test_ind_dict[1].update_age()
+        self.assertEqual(test_ind_dict[1].age, 149)
+        #Tests no birthdate
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, "1,1,2009" 
+        with self.assertRaises(AttributeError):
+            test_ind_dict[1].update_age()
+            CheckForErrors(test_ind_dict, {}).normal_age()
+        #Tests 200 years old with no death date
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, datetime.datetime.strptime("9 MAR 1818", "%d %b %Y").date() 
+        test_ind_dict[1].update_age()
+        with self.assertRaises(ValueError):
+            CheckForErrors(test_ind_dict, {}).normal_age()
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)

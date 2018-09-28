8473bd9752af5373c671dfc9d4147f1f7e8dee9d
Kyle Gensheimer <kylegensheimer@gmail.com>
2018-09-28 00:10:17 -0400

    Added US03 Test and function to generate an individual object
    Completed US03 testing. Instead of adding to the GEDCOM file to test each case, I created a function to easily an individual object which can be used to test:
    
    Created the gen_individual function in the unit test file.
    Parameters: gen_individual(self, name, sex, birt, deat)
    Instead of adding to the GEDCOM file to test each case, I created this function to easily generate an individual object which can be used to test. It doesn't take family into consideration.
    
    All unit tests are working and no longer interfere with eachother.
    I will work on creating a function to generate a family and the unit test
    for US04 tomorrow.

--- Unit_Test_Proj.py
+++ Unit_Test_Proj.py
@@ -3,9 +3,23 @@ from GedcomProject import AnalyzeGEDCOM, Family, Individual, CheckForErrors
 import datetime
 import os
 
+def gen_individual(self, name, sex, birt, deat):
+    """Generates an Individual object and returns it.
+    Parameters: name, sex, birt, deat"""
+    person = Individual()
+    person.name = name
+    person.sex = sex
+    person.birt = datetime.datetime.strptime(birt, "%d %b %Y").date()
+    if deat != None:
+        person.deat = datetime.datetime.strptime(deat, "%d %b %Y").date()
+        person.alive = True
+    else:
+        person.alive = False
+    return person
+
 class ProjectTest(unittest.TestCase):
     """Tests that our GEDCOM parser is working properly"""
-    
+
     def __init__(self, *args, **kwargs):
         super(ProjectTest, self).__init__(*args, **kwargs)
         cwd = os.path.dirname(os.path.abspath(__file__)) #gets directory of the file
@@ -20,36 +34,70 @@ class ProjectTest(unittest.TestCase):
         #self.num_of_errors += len(list_of_known_errors)
         for error in list_of_known_errors:
             self.assertIn(error, self.all_errors)
-        
+
 
     def test_normal_age(self):
-        """Tests that the normal_age method works properly, the first tests assertraises tests a bad 
+        """Tests that the normal_age method works properly, the first tests assertraises tests a bad
         inputs, the following checks are just based off fake dictionary info added"""
         #Tests age is 1000
         test_ind_dict = {1 : Individual()} #creates dictionary of individuals
-        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2001", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1001", "%d %b %Y").date() 
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2001", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1001", "%d %b %Y").date()
         test_ind_dict[1].update_age()
-        with self.assertRaises(ValueError): 
+        with self.assertRaises(ValueError):
             CheckForErrors(test_ind_dict, {}, False).normal_age()
         #Tests age is 150
-        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1850", "%d %b %Y").date() 
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1850", "%d %b %Y").date()
         test_ind_dict[1].update_age()
-        with self.assertRaises(ValueError):     
+        with self.assertRaises(ValueError):
             CheckForErrors(test_ind_dict, {}, False).normal_age()
         #Tests age is 149
-        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1851", "%d %b %Y").date() 
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", datetime.datetime.strptime("9 MAR 2000", "%d %b %Y").date(), datetime.datetime.strptime("9 MAR 1851", "%d %b %Y").date()
         test_ind_dict[1].update_age()
         self.assertEqual(test_ind_dict[1].age, 149)
         #Tests no birthdate
-        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, "1,1,2009" 
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, "1,1,2009"
         with self.assertRaises(AttributeError):
             test_ind_dict[1].update_age()
             CheckForErrors(test_ind_dict, {}, False).normal_age()
         #Tests 200 years old with no death date
-        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, datetime.datetime.strptime("9 MAR 1818", "%d %b %Y").date() 
+        test_ind_dict[1].name, test_ind_dict[1].deat, test_ind_dict[1].birt = "Fake Person", None, datetime.datetime.strptime("9 MAR 1818", "%d %b %Y").date()
         test_ind_dict[1].update_age()
         with self.assertRaises(ValueError):
             CheckForErrors(test_ind_dict, {}, False).normal_age()
+
+    def test_birth_before_death(self):
+        """US03: Unit Test: to ensure that birth occurs before the death of an individual"""
+        test_ind_dict = {}
+        # Test 1: Date of death is after date of birth, NO ERROR
+        test_ind_dict[1] = gen_individual(self, "Fake Person", "M", "26 FEB 1998", "26 FEB 1999")
+        try:
+            CheckForErrors(test_ind_dict, {}, False).birth_before_death()
+        except ValueError:
+            self.fail("test_birth_before_death() failed!")
+        # Test 2: Date of death is before date of birth, ERROR
+        test_ind_dict[1] = gen_individual(self, "Fake Person", "M", "26 FEB 1999", "26 FEB 1998")
+        with self.assertRaises(ValueError):
+            CheckForErrors(test_ind_dict, {}, False).birth_before_death()
+        # Test 3: Date of death is same day as birth, NO ERROR
+        test_ind_dict[1] = gen_individual(self, "Fake Person", "M", "26 FEB 1998", "26 FEB 1998")
+        try:
+            CheckForErrors(test_ind_dict, {}, False).birth_before_death()
+        except ValueError:
+            self.fail("test_birth_before_death() failed!")
+        # Test 4: Date of death is 1 day after birth, NO ERROR
+        test_ind_dict[1] = gen_individual(self, "Fake Person", "M", "26 FEB 1998", "27 FEB 1998")
+        try:
+            CheckForErrors(test_ind_dict, {}, False).birth_before_death()
+        except ValueError:
+            self.fail("test_birth_before_death() failed!")
+        # Test 5: Date of death is 1 day before birth, ERROR
+        test_ind_dict[1] = gen_individual(self, "Fake Person", "M", "27 FEB 1998", "26 FEB 1998")
+        with self.assertRaises(ValueError):
+            CheckForErrors(test_ind_dict, {}, False).birth_before_death()
+
+    #def test_marr_before_div(self):
+    #    """US04: Unit Test: to ensure that marriage dates come before divorce dates"""
+
 
     #def test_num_errors(self):
     #    """This checks to ensure the program did not catch any errors that we didn't test against"""
